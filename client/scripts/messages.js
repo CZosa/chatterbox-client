39b8077f964c1d15f3b49f190a260e1db3f96169
CZosa <zosachris@gmail.com>
2018-09-28 15:35:43 -0700

    created the message object via ajax method. andcompleted message.js

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,4 +1,11 @@
+/*
+<input type="text" name="message" id="message"/>
+
+*/
 var Messages = {
 
+  this.username = username;
+  this.text = $('#message').text();
+  this.roomname = roomname;
 
 };
--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -3,7 +3,22 @@ var Parse = {
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
   create: function(message, successCB, errorCB = null) {
-    // todo: save a message to the server
+    $.ajax({
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB || function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: errorCB || function(data) {
+        console.error('chatterbox: Failed to fetch messages', data);
+      }
+    })
+
+    //Try a test with JQuery version of Ajax post
+    //$.post(server, [message], [success fn], [datatype written as string i.e. 'object'])
+
   },
 
   readAll: function(successCB, errorCB = null) {
