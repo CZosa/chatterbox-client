151f4aacd37639ab5ebe7caee78a10ffa149bfc0
Ashvin Coca <AshvinCoca@gmail.com>
2018-09-29 12:17:46 -0700

    ommiting to testing branch

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -3,18 +3,7 @@ var Parse = {
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
   create: function(message, successCB, errorCB = null) {
-    $.ajax({
-      url: Parse.server,
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: successCB || function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: errorCB || function(data) {
-        console.error('chatterbox: Failed to fetch messages', data);
-      }
-    })
+
 
     //Try a test with JQuery version of Ajax post
     //$.post(server, [message], [success fn], [datatype written as string i.e. 'object'])
@@ -25,6 +14,7 @@ var Parse = {
     $.ajax({
       url: Parse.server,
       type: 'GET',
+      //we can use data as our limiter
       data: { order: '-createdAt' },
       contentType: 'application/json',
       success: successCB,
